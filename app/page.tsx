0bf78604cd6569321c84319a7a58554ca6e85b45
Norsang Nyandak <TNorsang>
2024-04-18 16:57:04 -0400

    svg icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+'use client'
+
 import Chat from '@/components/chat'
 import Image from 'next/image'
+import Robot from '@/components/ui/robot'
+import React, { useState } from 'react'
 
 export default function Home() {
+  const [showChat, setShowChat] = useState(false)
   return (
     <main className="flex flex-col h-screen items-center justify-center">
+      <div>
+        <Robot />
+      </div>
       <Chat />
     </main>
   )
