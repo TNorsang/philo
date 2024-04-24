2518891642337da9223325399ca1d99750b91649
TNorsang <thenorsang@gmail.com>
2024-04-24 16:38:32 -0700

    added logo

--- components/ui/nav.tsx
+++ components/ui/nav.tsx
@@ -1,9 +1,17 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function NavBar() {
   return (
     <nav className="flex justify-center space-x-28 items-center pt-8 pb-8 border-b-2 border-white bg-customOrange text-white rounded-lg text-2xl">
-      <Link className="home" href="/">
+      <Image
+        className="bg-blend-multiply"
+        src="/images/philo-logo.png"
+        alt="Logo"
+        width={50}
+        height={50}
+      />
+      <Link className="home " href="/">
         Home
       </Link>
       <Link className="chat" href="/chat">
