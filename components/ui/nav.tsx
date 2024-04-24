4d36f6c0fd8c18a89e6b8fb022713d9058e87460
TNorsang <thenorsang@gmail.com>
2024-04-24 16:27:42 -0700

    background etc

--- components/ui/nav.tsx
+++ components/ui/nav.tsx
@@ -3,8 +3,12 @@ import Link from 'next/link'
 export default function NavBar() {
   return (
     <nav className="flex justify-center space-x-28 items-center pt-8 pb-8 border-b-2 border-white bg-customOrange text-white rounded-lg text-2xl">
-      <Link href="/">Home</Link>
-      <Link href="/chat">Chat</Link>
+      <Link className="home" href="/">
+        Home
+      </Link>
+      <Link className="chat" href="/chat">
+        Chat
+      </Link>
     </nav>
   )
 }
