68433929579df6bbf646bc9cba7f08f05edc5f21
TNorsang <thenorsang@gmail.com>
2024-04-24 16:20:04 -0700

    changed background gradient

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const [showChat, setShowChat] = useState(false)
 
   return (
-    <main className="flex flex-col h-screen items-center justify-center bg-customOrange from-customOrange via-yellow-50 to-white">
+    <main className="flex flex-col h-screen items-center justify-center bg-gradient-to-b from-customOrange to-white">
       <h1>Hello</h1>
       <Link className="bg-blue-500" href="/chat">
         Go To Chat
