528c97a51e0b31b52b32747c0840c537222811bd
TNorsang <thenorsang@gmail.com>
2024-04-24 23:07:51 -0700

    main body

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useState, Suspense } from 'react'
 import Link from 'next/link'
+import { Loader } from '@react-three/drei'
+import { ReactDOM } from 'react'
+import { motion } from 'framer-motion'
 
 export default function Home() {
   const [showChat, setShowChat] = useState(false)
 
   return (
     <main className="flex flex-col h-screen items-center justify-center bg-gradient-to-b from-customOrange to-white">
-      <h1>Hello</h1>
-      <Link className="bg-blue-500" href="/chat">
-        Go To Chat
-      </Link>
+      <motion.div>
+        <h1 className="text-white text-6xl">Find Your Forever Friend Soon!</h1>
+      </motion.div>
     </main>
   )
 }
--- components/ui/nav.tsx
+++ components/ui/nav.tsx
@@ -6,32 +6,27 @@ import { motion } from 'framer-motion'
 
 export default function NavBar() {
   return (
-    <nav className="relative flex justify-center space-x-10 items-center pt-8 pb-8 border-b-2 border-white bg-customOrange text-white rounded-lg text-2xl ">
-      <ul className="inline-flex space-x-11 items-center justify-center sm:space-x-32">
-        <motion.li>
-          <motion.div
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            className="absolute left-4 top-4"
-          >
-            <Image
-              className="rounded-full"
-              src="/images/philo-logo.png"
-              alt="Logo"
-              width={60}
-              height={60}
-            />
-          </motion.div>
+    <nav className="relative flex justify-center items-center pt-6 pb-6 border-b-2 border-orange-200 bg-customOrange text-white text-3xl">
+      <ul className="flex items-center space-x-10 list-none">
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="absolute top-4 left-3 sm:top-3 sm:left-4"
+        >
+          <Image
+            className="rounded-full w-[50px] sm:w-[60px]"
+            src="/images/philo-logo.png"
+            alt="Logo"
+            width={60}
+            height={60}
+          />
+        </motion.div>
+        <motion.li className="hover:bg-white hover:bg-opacity-20 relative sm:right-16">
+          <Link href="/">Home</Link>
         </motion.li>
-
-        <motion.li className="hover:bg-white hover:bg-opacity-20 focus:bg-white focus:bg-opacity-20">
-          <motion.div>
-            <Link href="/">Home</Link>
-          </motion.div>
-        </motion.li>
-        <li className="hover:bg-white hover:bg-opacity-20 focus:bg-white focus:bg-opacity-20">
+        <motion.li className="hover:bg-white hover:bg-opacity-20 relative sm:left-7">
           <Link href="/chat">Chat</Link>
-        </li>
+        </motion.li>
       </ul>
     </nav>
   )
