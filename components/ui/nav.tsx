dbc5a0dc79f6c7767c64d8ac8f3284328da26ebb
TNorsang <thenorsang@gmail.com>
2024-04-24 21:47:29 -0700

    fixed nav responsiveness

--- components/ui/nav.tsx
+++ components/ui/nav.tsx
@@ -1,19 +1,29 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
+import { motion } from 'framer-motion'
 
 export default function NavBar() {
   return (
-    <nav className="flex justify-center space-x-10 items-center pt-8 pb-8 border-b-2 border-white bg-customOrange text-white rounded-lg text-2xl ">
+    <nav className="relative flex justify-center space-x-10 items-center pt-8 pb-8 border-b-2 border-white bg-customOrange text-white rounded-lg text-2xl ">
       <ul className="inline-flex space-x-11 items-center justify-center sm:space-x-32">
-        <li className="w-full">
-          <Image
-            className="rounded-full"
-            src="/images/philo-logo.png"
-            alt="Logo"
-            width={50}
-            height={50}
-          />
-        </li>
+        <motion.li>
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="absolute left-4 top-4"
+          >
+            <Image
+              className="rounded-full"
+              src="/images/philo-logo.png"
+              alt="Logo"
+              width={60}
+              height={60}
+            />
+          </motion.div>
+        </motion.li>
+
         <li className="hover:bg-white hover:bg-opacity-20 focus:bg-white focus:bg-opacity-20">
           <Link href="/">Home</Link>
         </li>
