260b890e8e5f6c4abe201bc7cbe6df682ee931d5
TNorsang <thenorsang@gmail.com>
2024-04-25 19:29:29 -0700

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,24 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
 import PhiloModel from '@/components/ui/philoModel'
+import dynamic from 'next/dynamic'
 
 export default function Home() {
+  const My3DModel = dynamic(() => import('../components/ui/philoModel'), {
+    ssr: false,
+  }) // Client-side rendering
+
   const [showChat, setShowChat] = useState(false)
 
   return (
     <main className="flex flex-col h-screen items-center justify-center bg-gradient-to-b from-customOrange to-customLightOrange">
-      <PhiloModel />
-      <motion.div>
-        <h1 className="text-white text-6xl">Meet Your Forever Friend</h1>
+      <motion.div className=" mt-28 sm:mt-24">
+        <h1 className="text-white text-3xl sm:text-6xl">
+          Meet Your Forever Friend
+        </h1>
       </motion.div>
+
+      <PhiloModel />
     </main>
   )
 }
--- components/ui/philoModel.tsx
+++ components/ui/philoModel.tsx
@@ -1,17 +1,19 @@
+'cient side'
+
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, useGLTF } from '@react-three/drei'
 
 const PhiloModel = () => {
   const { scene } = useGLTF('/models/philo.glb')
   return (
-    <Canvas style={{ width: '100%', height: '400px' }}>
-      <ambientLight intensity={0.3} />
-      <directionalLight position={[5, 10, 7.5]} intensity={0.8} />{' '}
+    <Canvas className="w-2 h-2">
+      <ambientLight intensity={0.3} /> {/* Low-level ambient light */}
+      <directionalLight position={[5, 10, 7.5]} intensity={0.9} />
       {/* Mimic sunlight */}
       <pointLight position={[-5, -10, -7.5]} intensity={0.5} />{' '}
       {/* Additional point light */}
       <OrbitControls />
-      <primitive object={scene} scale={0.1} /> {/* Scale down to 10% */}
+      <primitive object={scene} scale={0.2} /> {/* Scale down to 10% */}
     </Canvas>
   )
 }
