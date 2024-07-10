72bf410f63339ab916eaea491cae740995ddce07
Norsang Nyandak <TNorsang>
2024-07-10 10:54:43 -0400

    deploy test

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
-// import Burger from '../svgs/Burger'
+import Burger from '../svgs/Burger'
 import { useState } from 'react'
 
 export default function NavBar() {
