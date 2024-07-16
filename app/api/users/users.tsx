548ab3f3746e9d90172c36c2de356cd7cb74cf56
TNorsang <thenorsang@gmail.com>
2024-07-16 00:45:07 -0400

    gave type to res,req

--- app/api/users/users.tsx
+++ app/api/users/users.tsx
@@ -1,8 +1,12 @@
 import { PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next'
 
 const prisma = new PrismaClient()
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   if (req.method === 'GET') {
     const users = await prisma.user.findMany()
     res.status(200).json(users)
