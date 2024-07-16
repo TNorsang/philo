17fe5850104e5f1298ddc282509bfe22a6deaadb
TNorsang <thenorsang@gmail.com>
2024-07-16 01:03:41 -0400

    Layouts

--- app/page.tsx
+++ app/page.tsx
@@ -33,20 +33,20 @@ export default function Home() {
     <main className="relative flex justify-center items-center w-screen h-screen overflow-hidden">
       {/* <FriendlyLandingPage /> */}
       <div className="relative z-10">{showCursor && <Cursor />}</div>
-      <motion.div className="flex border-4">
+      <motion.div className="flex">
         <motion.div className="top-[152px] left-[40px] sm:left-[368px] sm:top-[184px] z-20">
-          <h1 className="text-white text-[48px] font-semibold w-[252px] h-[177px] md:text-[88px] md:w-[464px] md:h-[304px] relative left-12">
+          <h1 className="text-white font-semibold text-[40px] w-[200px] sm:text-[60px] sm:w-[300px] md:text-[88px] md:w-[450px] md:left-4 relative left-8">
             Meet Your Forever Friend
           </h1>
         </motion.div>
-        <motion.div className="flex flex-col items-center relative right-5">
+        <motion.div className="flex flex-col items-center relative right-5 top-10 sm:top-14 md:top-20">
           <motion.div
             initial={{ opacity: 0, y: -100 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 4 }}
           >
             <Image
-              className="top-[235px] right-[32px] md:top-[264px] md:right-[312px] md:w-[313px] md:h-[344px]"
+              className="top-[235px] right-[32px] md:top-[264px] md:right-[312px] md:w-[313px] md:h-[344px] sm:w-[190px] sm:h-[212px]"
               alt="Bay Max"
               src="/images/bmax.png"
               width={172}
@@ -56,7 +56,7 @@ export default function Home() {
 
           <motion.div
             whileHover={{ scale: 1.03 }}
-            className="bg-gradient-to-b from-white to-gray-300 w-[200px] h-[80px] flex justify-center items-center right-[16px] sm:right-[368px] top-[424px] sm:top-[608px] text-[24px] font-semibold text-customFontOrange z-50"
+            className="bg-gradient-to-b from-white to-gray-300 w-[170px] h-[60px] flex justify-center items-center right-[16px] top-[424px] sm:right-[368px] sm:top-[608px] text-[20px] font-semibold text-customFontOrange z-50"
           >
             <Link href={signedIn ? '/chat' : '/signin'}>
               <button>Get Started</button>
