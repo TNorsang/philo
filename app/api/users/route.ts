ec78da29c892b9a513da8ce094d1eedcdeabaa8a
TNorsang <thenorsang@gmail.com>
2024-07-18 18:29:11 -0400

    Successfully stored user information to postgres database

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,9 +1,57 @@
 import { prisma } from '@/lib/db'
 import { NextResponse } from 'next/server'
+import { hash } from 'bcrypt'
 
-export async function GET(req: Request) {
-  const users = await NextResponse.json({
-    message: 'Hello World',
-  })
-  return users
+export async function POST(req: Request) {
+  try {
+    const body = await req.json()
+    const { email, username, password } = body
+
+    // Email exists?
+    const existingEmail = await prisma.user.findUnique({
+      where: { email: email },
+    })
+    if (existingEmail)
+      return NextResponse.json(
+        { user: null, message: 'Email already exists' },
+        { status: 409 },
+      )
+
+    // Username exists?
+    const existingUsername = await prisma.user.findUnique({
+      where: { username: username },
+    })
+    if (existingUsername)
+      return NextResponse.json(
+        {
+          user: null,
+          message: 'Username already exists',
+        },
+        { status: 409 },
+      )
+
+    const hashedPassword = await hash(password, 10)
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    })
+
+    const { password: newUserPassword, ...rest } = newUser
+
+    return NextResponse.json(
+      {
+        user: newUser,
+        message: 'User created successfully',
+      },
+      { status: 201 },
+    )
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'Something went wrong' },
+      { status: 500 },
+    )
+  }
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 
 export default function SignUp() {
   const [user, setUser] = useState({ username: '', email: '', password: '' })
+  const [signedIn, setSignedIn] = useState(false)
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
@@ -23,6 +24,7 @@ export default function SignUp() {
 
       if (res.ok) {
         alert('User Created Successfully!')
+        setSignedIn(true)
       } else {
         alert('Error Creating User!')
       }
@@ -34,6 +36,7 @@ export default function SignUp() {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-opacity-50">
+      {signedIn ? <h1> Welcome! {user.username} </h1> : <h1> </h1>}
       <div className="flex flex-col items-center gap-4 bg-opacity-50 bg-white p-8 rounded-lg shadow-lg w-full max-w-4xl">
         <input
           className="w-5/12 rounded-full h-[56px] border-4 p-4 shadow-xl text-[16px] mb-4"
