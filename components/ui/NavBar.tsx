eaa0226e8b1d8d4467d75b23890fd5d2a1124486
TNorsang <thenorsang@gmail.com>
2024-08-02 16:45:07 -0400

    updated navbar layout

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -20,36 +20,36 @@ export default function NavBar() {
           <motion.div
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
-            className="absolute top-[32px] left-[24px]"
+            className="absolute top-[34px] left-[32px]"
           >
             <Image
               className="rounded-full"
-              src="/images/bmax_logo.png"
+              src="/images/philo-logo.png"
               alt="Logo"
               width={48}
               height={48}
             />
           </motion.div>
         </Link>
-        <motion.div className="absolute left-[80px] top-[32px] text-[32px] font-bold text-white">
+        <motion.div className="absolute left-[84px] top-[36px] text-[32px] font-bold text-white">
           Philo
         </motion.div>
 
         <motion.li
           whileHover={{ scale: 1.1 }}
-          className="hidden sm:block absolute right-[80px] top-[40px] text-[24px] font-bold text-white"
+          className="hidden sm:block absolute right-[80px] top-[44px] text-[19px] font-bold text-white"
         >
           <Link href="/">HOME</Link>
         </motion.li>
         <motion.li
           whileHover={{ scale: 1.1 }}
-          className="hidden sm:block absolute right-[192px] top-[40px] text-[24px] font-bold text-white"
+          className="hidden sm:block absolute right-[189px] top-[44px] text-[19px] font-bold text-white"
         >
           <Link href="/chat">CHAT</Link>
         </motion.li>
         <motion.li
           whileHover={{ scale: 1.1 }}
-          className="hidden sm:block absolute right-[296px] top-[40px] text-[24px] font-bold text-white"
+          className="hidden sm:block absolute right-[286px] top-[44px] text-[19px] font-bold text-white"
         >
           <Link href="/signin">SIGN IN</Link>
         </motion.li>
