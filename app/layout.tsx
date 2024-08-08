7fd20463c016db3a148bafce45148d11b911f9f4
TNorsang <thenorsang@gmail.com>
2024-08-07 21:58:41 -0400

    added analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
           </div>
           <div className="overflow-y-hidden">{children}</div>
           <UnderMaintenance />
+          <Analytics />
         </SessionProvider>
       </body>
     </html>
